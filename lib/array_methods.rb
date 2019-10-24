3bc64d9082a69b4d65f5c18f8e133423b7c0aaff
Maika Kailani <queenmab051@gmail.com>
2019-10-24 05:59:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max = 0
+  array.length.times { |index|
+    if array[index] > max
+      max = array[index]
+  }
+  puts max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = 20
+  array.length.times { |index|
+    if array[index] < min
+      min = array[index]
+  }
+  puts min
 end
